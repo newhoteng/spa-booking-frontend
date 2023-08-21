f6067ab24adf1b94d8634d5fcc781bff7f428718
fatiha <fathiato@gmail.com>
2023-08-21 14:26:48 +0100

    rearranging services

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchAllServices } from '../redux/serviceSlice';
-// import PropTypes from 'prop-types';
 import Services from './Services';
 
 const HomePage = () => {
@@ -10,37 +9,38 @@ const HomePage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchAllServices());
-  }, []);
+    setTimeout(() => { dispatch(fetchAllServices()); }, 5000);
+  }, [dispatch]);
 
   return (
     <main className="home">
       <h1>Spa Services</h1>
-      {isLoading && <h1>Loading...</h1>}
-      {isError && <h1>Something went wrong please reload the page...</h1>}
-      <p className="home-font">Please select a service or two!</p>
-      {services.length === 0 && (
+      {!isLoading && services.length === 0 && (
         <p className="home-font">
           No available service. Please add a service to view.
         </p>
       )}
-      <ul className="service-ul">
-        {services.map((service) => (
-          <Services
-            key={service.id}
-            name={service.name}
-            image={service.image}
-            description={service.description}
-          />
-        ))}
-      </ul>
+      {isError && <h1>Something went wrong please reload the page...</h1>}
+      {isLoading ? <h1>Loading...</h1>
+        : (
+          <div>
+            <p className="home-font">Please select a service or two!</p>
+            <ul className="service-ul">
+              {services.map((service) => (
+                <Services
+                  key={service.id}
+                  to={service}
+                  name={service.name}
+                  image={service.image}
+                  description={service.description}
+                />
+              ))}
+            </ul>
+          </div>
+
+        )}
     </main>
   );
 };
 
-// HomePage.propTypes = {
-//   // eslint-disable-next-line react/forbid-prop-types
-//   services: PropTypes.array.isRequired,
-//   // setServices: PropTypes.string.isRequired,
-// };
 export default HomePage;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,19 +1,24 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import { CiFacebook, CiTwitter, CiInstagram } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 
-const Services = ({ name, image, description }) => (
+const Services = ({
+  name, to, image, description,
+}) => (
   <div>
-    <li className="services">
-      <div className="image">
-        <img src={`${image}`} alt="service" />
-      </div>
-      <h2 className="header-font">{name}</h2>
-      <p className="p-font">
-        {description.substring(0, 20)}
-        ...
-      </p>
-    </li>
+    <Link to={to} className="services">
+      <li className="services">
+        <div className="image">
+          <img src={`${image}`} alt="service" />
+        </div>
+        <h2 className="header-font">{name}</h2>
+        <p className="p-font">
+          {description.substring(0, 20)}
+          ...
+        </p>
+      </li>
+    </Link>
     <div className="icons">
       <CiFacebook />
       <CiTwitter />
@@ -24,6 +29,7 @@ const Services = ({ name, image, description }) => (
 Services.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
 export default Services;
