dd13f0360953aae2f12a6cf24fc9735c7425c934
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-22 07:26:27 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import 'react-toastify/dist/ReactToastify.css';
-import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reservationsReducer from './reservations/reservationsSlice';
-import usersReducer from './users/usersSlice';
 import servicesReducer from './serviceSlice';
 
 const store = configureStore({
   reducer: {
     userReservations: reservationsReducer,
-    currentUser: usersReducer,
     services: servicesReducer,
   },
 });
